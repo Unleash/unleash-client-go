cd1b11bd25274517db2a24d58d0d3e6586765e25
Jason Barron <jasonrbarron@gmail.com>
2017-03-07 20:36:51 +0100

    Add tests for Default.

---
+++ internal/strategies/default_test.go
@@ -0,0 +1,20 @@
+package strategies
+
+import "testing"
+
+func TestDefaultStrategy_Name(t *testing.T) {
+	strategy := NewDefaultStrategy()
+
+	if strategy.Name() != "default" {
+		t.Errorf("strategy should have correct name: %s", strategy.Name())
+	}
+}
+
+func TestDefaultStrategy_IsEnabled(t *testing.T) {
+	s := NewDefaultStrategy()
+
+	if !s.IsEnabled(nil,nil) {
+	t.Errorf("default strategy should be enabled")
+	}
+
+}
