e749045c72fce6f4e3f5108530f3decc2f1297a0
Jason Barron <jasonrbarron@gmail.com>
2017-07-11 09:29:09 +0200

    Introduce some autotests for testing the metrics
    After playing with different testing frameworks, the combo of
    Gock/Testify seems to work quite well so let's use that for
    asserting and mocking.

---
+++ metrics_test.go
@@ -0,0 +1,140 @@
+package unleash
+
+import (
+	"github.com/Unleash/unleash-client-go/internal/api"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"gopkg.in/h2non/gock.v1"
+	"net/url"
+	"testing"
+	"time"
+)
+
+const (
+	server     = "http://foo.com"
+	appName    = "unleash-client-go-tests"
+	instanceID = "1234"
+)
+
+func TestMetrics_RegisterInstance(t *testing.T) {
+	assert := assert.New(t)
+	defer gock.Off()
+
+	gock.New(server).
+		Post("/client/register").
+		MatchHeader("UNLEASH-APPNAME", appName).
+		MatchHeader("UNLEASH-INSTANCEID", instanceID).
+		Reply(200)
+
+	client, err := NewClient(
+		WithUrl(server),
+		WithAppName(appName),
+		WithInstanceId(instanceID),
+	)
+
+	assert.Nil(err, "client should not return an error")
+	data := <-client.registered
+	assert.NotNil(data, "client data should not be nil")
+	assert.True(gock.IsDone(), "there should be no more mocks")
+}
+
+func TestMetrics_DoPost(t *testing.T) {
+	assert := assert.New(t)
+	defer gock.Off()
+
+	gock.New(server).
+		Post("/client/register").
+		Reply(200)
+
+	gock.New(server).
+		Post("").
+		MatchHeader("UNLEASH-APPNAME", appName).
+		MatchHeader("UNLEASH-INSTANCEID", instanceID).
+		Reply(200)
+
+	client, err := NewClient(
+		WithUrl(server),
+		WithAppName(appName),
+		WithInstanceId(instanceID),
+	)
+
+	assert.Nil(err, "client should not return an error")
+
+	m := client.metrics
+
+	serverUrl, _ := url.Parse(server)
+	res, err := m.doPost(serverUrl, &struct{}{})
+
+	assert.Nil(err, "doPost should not return an error")
+	assert.Equal(200, res.StatusCode, "statusCode should be 200")
+	assert.True(gock.IsDone(), "there should be no more mocks")
+}
+
+func TestMetrics_SendMetrics(t *testing.T) {
+	assert := assert.New(t)
+	defer gock.Off()
+
+	gock.New(server).
+		Post("/client/register").
+		Reply(200)
+
+	gock.New(server).
+		Get("/features").
+		Reply(200).
+		JSON(api.FeatureResponse{
+			Response: api.Response{
+				Version: 1,
+			},
+			Features: []api.Feature{
+				{
+					Name:    "foo",
+					Enabled: true,
+					Strategies: []api.Strategy{
+						{
+							Name: "default",
+						},
+					},
+				},
+			},
+		})
+
+	gock.New(server).
+		Post("/client/metrics").
+		MatchHeader("UNLEASH-APPNAME", appName).
+		MatchHeader("UNLEASH-INSTANCEID", instanceID).
+		Times(5).
+		Reply(200)
+
+	mockListener := &MockedListener{}
+
+	mockListener.On("OnReady").Return()
+	mockListener.On("OnRegistered", mock.AnythingOfType("ClientData"))
+	mockListener.On("OnCount", mock.AnythingOfType("string"), mock.AnythingOfType("bool"))
+	mockListener.On("OnSent", mock.AnythingOfType("MetricsData"))
+
+	client, err := NewClient(
+		WithUrl(server),
+		WithAppName(appName),
+		WithInstanceId(instanceID),
+		WithMetricsInterval(200*time.Millisecond),
+		WithListener(mockListener),
+	)
+	assert.Nil(err, "client should not return an error")
+
+	for i := 0; i < 20; i++ {
+		client.IsEnabled("foo")
+
+		if i%5 == 0 {
+			time.Sleep(1 * time.Second)
+		}
+	}
+
+	time.Sleep(1 * time.Second)
+
+	mockListener.AssertNumberOfCalls(t, "OnRegistered", 1)
+	mockListener.AssertNumberOfCalls(t, "OnReady", 1)
+	mockListener.AssertNumberOfCalls(t, "OnCount", 20)
+	mockListener.AssertNumberOfCalls(t, "OnSent", 5)
+
+	assert.True(gock.IsDone(), "there should be no more mocks")
+}
