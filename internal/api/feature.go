de2dce306d00d8f7b431dac471f013b3c7769a7f
Claire Lee <claire.lee@sensestar.live>
2020-12-06 00:36:51 +0800

    Implement variants

--- internal/api/feature.go
+++ internal/api/feature.go
@@ -1,6 +1,14 @@
 package api
 
-import "time"
+import (
+	"time"
+	"math/rand"
+	"strconv"
+	"fmt"
+
+	"github.com/Unleash/unleash-client-go/v3/context"
+	"github.com/spaolacci/murmur3"
+)
 
 type ParameterMap map[string]interface{}
 
@@ -17,6 +25,7 @@ type Feature struct {
 	CreatedAt   time.Time    `json:"createdAt"`
 	Strategy    string       `json:"strategy"`
 	Parameters  ParameterMap `json:"parameters"`
+	Variants	[]Variant    `json:"variants"`
 }
 
 func (fr FeatureResponse) FeatureMap() map[string]interface{} {
@@ -25,4 +34,44 @@ func (fr FeatureResponse) FeatureMap() map[string]interface{} {
 		features[f.Name] = f
 	}
 	return features
+}
+
+func (f Feature) getVariantFromWeights(ctx *context.Context) *Variant {
+	if len(f.Variants) > 0 {
+		totalWeight := 0
+		for _, variant := range f.Variants {
+			totalWeight += variant.Weight
+		}
+		if totalWeight == 0 {
+			return DISABLED_VARIANT
+		}
+
+		target := getNormalizedNumber(getSeed(ctx), f.Name, totalWeight)
+
+		counter := uint32(0)
+		for _, variant := range f.Variants {
+			counter += uint32(variant.Weight)
+			
+			if counter >= target {
+				return &variant
+			}
+		}
+	}
+	return DISABLED_VARIANT
+}
+
+func getSeed(ctx *context.Context) string {
+	if ctx.UserId != "" {
+		return ctx.UserId
+	} else if ctx.SessionId != "" {
+		return ctx.SessionId
+	} else if ctx.RemoteAddress != "" {
+		return ctx.RemoteAddress
+	} else {
+		return strconv.Itoa(rand.Intn(10000))
+	}
+}
+
+func getNormalizedNumber(identifier string, groupId string, normalizer int) uint32 {
+	return (murmur3.Sum32([]byte(fmt.Sprintf("%s:%s", identifier, groupId))) % uint32(normalizer)) + 1
 }
