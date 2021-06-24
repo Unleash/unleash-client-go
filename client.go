35c2cad706100f534f5f9086876bb5123f9fd0a5
Fredrik Oseberg <fredrik.no@gmail.com>
2021-06-24 10:43:16 +0200

    chore: update client version

--- client.go
+++ client.go
@@ -17,7 +17,7 @@ import (
 const (
 	deprecatedSuffix = "/features"
 	clientName       = "unleash-client-go"
-	clientVersion    = "3.2.1"
+	clientVersion    = "3.2.2"
 )
 
 var defaultStrategies = []strategy.Strategy{
