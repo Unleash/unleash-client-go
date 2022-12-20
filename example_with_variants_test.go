e5b258adb69e2c074a46fde38469577d3da4d81d
Gastón Fournier <gaston@getunleash.ai>
2022-12-20 12:44:19 +0100

    Add more tests

--- example_with_variants_test.go
+++ example_with_variants_test.go
@@ -39,3 +39,28 @@ func Test_withVariants(t *testing.T) {
 		t.Fatalf("Expected one of the variant payloads")
 	}
 }
+
+func Test_withVariantsAndUnexistentUnexistingStrategy(t *testing.T) {
+	demoReader, err := os.Open("demo_app_toggles.json")
+	if err != nil {
+		t.Fail()
+	}
+	err = unleash.Initialize(
+		unleash.WithListener(&unleash.DebugListener{}),
+		unleash.WithAppName("my-application"),
+		unleash.WithRefreshInterval(5*time.Second),
+		unleash.WithMetricsInterval(5*time.Second),
+		unleash.WithStorage(&unleash.BootstrapStorage{Reader: demoReader}),
+		unleash.WithUrl("https://localhost:4242"),
+	)
+
+	if err != nil {
+		t.Fail()
+	}
+
+	feature := unleash.GetVariant("AuditLog")
+	fmt.Printf("feature is %v\n", feature)
+	if feature.Enabled == true {
+		t.Fatalf("Expected feature to be disabled because Environment does not exist as strategy")
+	}
+}
