c68ff30e726446d983a1f1f040ee435e1d11fe16
Gastón Fournier <gaston@getunleash.ai>
2022-12-21 09:56:28 +0100

    Re-add tests removed from example_ file

---
+++ unleash_test.go
@@ -0,0 +1,66 @@
+package unleash_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/Unleash/unleash-client-go/v3"
+)
+
+func Test_withVariants(t *testing.T) {
+	demoReader, err := os.Open("demo_app_toggles.json")
+	if err != nil {
+		t.Fail()
+	}
+	err = unleash.Initialize(
+		unleash.WithListener(&unleash.DebugListener{}),
+		unleash.WithAppName("my-application"),
+		unleash.WithRefreshInterval(5*time.Second),
+		unleash.WithMetricsInterval(5*time.Second),
+		unleash.WithStorage(&unleash.BootstrapStorage{Reader: demoReader}),
+		unleash.WithUrl("https://localhost:4242"),
+	)
+
+	if err != nil {
+		t.Fail()
+	}
+
+	feature := unleash.GetVariant("Demo")
+	fmt.Printf("feature is %v\n", feature)
+	if feature.Enabled == false {
+		t.Fatalf("Expected feature to be enabled")
+	}
+	if feature.Name != "small" && feature.Name != "medium" {
+		t.Fatalf("Expected one of the variant names")
+	}
+	if feature.Payload.Value != "35" && feature.Payload.Value != "55" {
+		t.Fatalf("Expected one of the variant payloads")
+	}
+}
+
+func Test_withVariantsAndANonExistingStrategyName(t *testing.T) {
+	demoReader, err := os.Open("demo_app_toggles.json")
+	if err != nil {
+		t.Fail()
+	}
+	err = unleash.Initialize(
+		unleash.WithListener(&unleash.DebugListener{}),
+		unleash.WithAppName("my-application"),
+		unleash.WithRefreshInterval(5*time.Second),
+		unleash.WithMetricsInterval(5*time.Second),
+		unleash.WithStorage(&unleash.BootstrapStorage{Reader: demoReader}),
+		unleash.WithUrl("https://localhost:4242"),
+	)
+
+	if err != nil {
+		t.Fail()
+	}
+
+	feature := unleash.GetVariant("AuditLog")
+	fmt.Printf("feature is %v\n", feature)
+	if feature.Enabled == true {
+		t.Fatalf("Expected feature to be disabled because Environment does not exist as strategy")
+	}
+}
