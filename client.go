ebfcf13a52ddbfcb6c4fe96afb4b713acad25739
Gastón Fournier <gaston@getunleash.ai>
2022-12-22 10:21:26 +0100

    fix: bump client version to 3.7.2 (#126)

--- client.go
+++ client.go
@@ -17,7 +17,7 @@ import (
 const (
 	deprecatedSuffix = "/features"
 	clientName       = "unleash-client-go"
-	clientVersion    = "3.7.1"
+	clientVersion    = "3.7.2"
 )
 
 var defaultStrategies = []strategy.Strategy{
