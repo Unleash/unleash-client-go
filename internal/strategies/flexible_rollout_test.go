4baecaddb6a7a17fc9f6c3309484b5f481d3a5cf
Simon Hornby <liquidwicked64@gmail.com>
2023-04-04 09:11:06 +0200

    fix: patch random stickiness calculations for rollout strategies (#131)

---
+++ internal/strategies/flexible_rollout_test.go
@@ -0,0 +1,32 @@
+package strategies
+
+import (
+	"testing"
+
+	"github.com/Unleash/unleash-client-go/v3/context"
+	"github.com/Unleash/unleash-client-go/v3/strategy"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFlexibleRolloutStrategy_IsWellDistributed(t *testing.T) {
+	s := NewFlexibleRolloutStrategy()
+
+	enabledCount := 0
+	rounds := 200000
+
+	for i := 0; i < rounds; i++ {
+		params := map[string]interface{}{
+			strategy.ParamStickiness: "random",
+			strategy.ParamRollout:    50,
+			strategy.ParamGroupId:    "test51",
+		}
+		enabled := s.IsEnabled(params, &context.Context{})
+		if enabled {
+			enabledCount++
+		}
+	}
+
+	actualPercentage := round(100.0 * float64(enabledCount) / float64(rounds))
+
+	assert.InDelta(t, 50, actualPercentage, 1.0)
+}
