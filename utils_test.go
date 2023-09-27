94ece706f3f642506458c34269c8c200c61d817b
Fredrik Oseberg <fredrik.no@gmail.com>
2023-09-27 16:03:21 +0200

    fix: add more test cases

--- utils_test.go
+++ utils_test.go
@@ -15,45 +15,90 @@ func TestGetFetchURLPath(t *testing.T) {
 	res = getFetchURLPath("myProject")
 	assert.Equal("./client/features?project=myProject", res)
 }
+
 func TestEvery(t *testing.T) {
-	// Test case 1: Check if all integers in the slice are even.
-	numbers1 := []int{2, 4, 6, 8, 10}
-	allEven := every(numbers1, func(item interface{}) bool {
-		num, ok := item.(int)
-		if !ok {
-			t.Errorf("Expected an integer, got %T", item)
-			return false
+	t.Run("All Even Integers", func(t *testing.T) {
+		numbers := []int{2, 4, 6, 8, 10}
+		allEven := every(numbers, func(item interface{}) bool {
+			num, ok := item.(int)
+			if !ok {
+				t.Errorf("Expected an integer, got %T", item)
+				return false
+			}
+			return num%2 == 0
+		})
+		if !allEven {
+			t.Errorf("Expected all numbers to be even, but got false")
 		}
-		return num%2 == 0
 	})
-	if !allEven {
-		t.Errorf("Expected all numbers to be even, but got false")
-	}
 
-	// Test case 2: Check if all strings in the slice have more than 3 characters.
-	words := []string{"apple", "banana", "cherry"}
-	allLong := every(words, func(item interface{}) bool {
-		str, ok := item.(string)
-		if !ok {
-			t.Errorf("Expected a string, got %T", item)
-			return false
+	t.Run("All Long Strings", func(t *testing.T) {
+		words := []string{"apple", "banana", "cherry"}
+		allLong := every(words, func(item interface{}) bool {
+			str, ok := item.(string)
+			if !ok {
+				t.Errorf("Expected a string, got %T", item)
+				return false
+			}
+			return len(str) > 3
+		})
+		if !allLong {
+			t.Errorf("Expected all words to be long, but got false")
 		}
-		return len(str) > 3
-	})
-	if !allLong {
-		t.Errorf("Expected all words to be long, but got false")
-	}
-
-	// Test case 3: Check an empty slice.
-	emptySlice := []int{}
-	allEmpty := every(emptySlice, func(item interface{}) bool {
-		// This condition should not be reached for an empty slice.
-		t.Errorf("Unexpected condition reached")
-		return false
 	})
 
-	if allEmpty {
-		t.Errorf("Expected an empty slice to return false, but got true")
-	}
+	t.Run("Empty Slice", func(t *testing.T) {
+		emptySlice := []int{}
+		allEmpty := every(emptySlice, func(item interface{}) bool {
+			// This condition should not be reached for an empty slice.
+			t.Errorf("Unexpected condition reached")
+			return false
+		})
+
+		if allEmpty {
+			t.Errorf("Expected an empty slice to return false, but got true")
+		}
+	})
+
+	t.Run("invalid inout", func(t *testing.T) {
+		invalidInput := "string"
+		result := every(invalidInput, func(item interface{}) bool {
+			// This condition should not be reached for an empty slice.
+			return true
+		})
+
+		if result == true {
+			t.Errorf("Expected result to be false")
+		}
+	})
+}
+
+func TestContains(t *testing.T) {
+	t.Run("Element is present in the slice", func(t *testing.T) {
+		arr := []string{"apple", "banana", "cherry", "date", "fig"}
+		str := "banana"
+		result := contains(arr, str)
+		if !result {
+			t.Errorf("Expected '%s' to be in the slice, but it was not found", str)
+		}
+	})
+
+	t.Run("Element is not present in the slice", func(t *testing.T) {
+		arr := []string{"apple", "banana", "cherry", "date", "fig"}
+		str := "grape"
+		result := contains(arr, str)
+		if result {
+			t.Errorf("Expected '%s' not to be in the slice, but it was found", str)
+		}
+	})
+
+	t.Run("Empty slice should return false", func(t *testing.T) {
+		arr := []string{}
+		str := "apple"
+		result := contains(arr, str)
+		if result {
+			t.Errorf("Expected an empty slice to return false, but it returned true")
+		}
+	})
 }
 
