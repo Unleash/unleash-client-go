7de33fa87d2fe8d64a561bd04cad889edc896d72
Christopher Kolstad <chriswk@getunleash.io>
2023-12-06 13:06:59 +0100

    chore: clean tests usage of endpoints
    Previously, once we hit the feature refresh interval, we saw failed
    requests against https://localhost:4242, this update replaces all usages
    in these tests with the gocked url instead of a fake localhost address

--- unleash_test.go
+++ unleash_test.go
@@ -2,15 +2,32 @@ package unleash_test
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"testing"
 	"time"
 
 	"github.com/Unleash/unleash-client-go/v4"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/h2non/gock.v1"
 )
 
 func Test_withVariants(t *testing.T) {
+	a := assert.New(t)
 	demoReader, err := os.Open("demo_app_toggles.json")
+	defer gock.OffAll()
+
+	gock.New("http://foo.com").
+		Post("/client/register").
+		Reply(200)
+		// Read the file into a byte slice
+	byteValue, _ := io.ReadAll(demoReader)
+	// Convert the byte slice to a string
+	jsonStr := string(byteValue)
+
+	// Use the string as the body of the Gock request
+	gock.New("http://foo.com").
+		Get("/client/features").Reply(200).BodyString(jsonStr)
 	if err != nil {
 		t.Fail()
 	}
@@ -18,9 +35,9 @@ func Test_withVariants(t *testing.T) {
 		unleash.WithListener(&unleash.DebugListener{}),
 		unleash.WithAppName("my-application"),
 		unleash.WithRefreshInterval(5*time.Second),
-		unleash.WithMetricsInterval(5*time.Second),
+		unleash.WithDisableMetrics(true),
 		unleash.WithStorage(&unleash.BootstrapStorage{Reader: demoReader}),
-		unleash.WithUrl("https://localhost:4242"),
+		unleash.WithUrl("http://foo.com"),
 	)
 
 	if err != nil {
@@ -38,20 +55,36 @@ func Test_withVariants(t *testing.T) {
 	if feature.Payload.Value != "35" && feature.Payload.Value != "55" {
 		t.Fatalf("Expected one of the variant payloads")
 	}
+	err = unleash.Close()
+	a.Nil(err)
 }
 
 func Test_withVariantsAndANonExistingStrategyName(t *testing.T) {
+	a := assert.New(t)
 	demoReader, err := os.Open("demo_app_toggles.json")
 	if err != nil {
 		t.Fail()
 	}
+	defer gock.OffAll()
+
+	gock.New("http://foo.com").
+		Post("/client/register").
+		Reply(200)
+
+	byteValue, _ := io.ReadAll(demoReader)
+	// Convert the byte slice to a string
+	jsonStr := string(byteValue)
+
+	// Use the string as the body of the Gock request
+	gock.New("http://foo.com").
+		Get("/client/features").Reply(200).BodyString(jsonStr)
 	err = unleash.Initialize(
 		unleash.WithListener(&unleash.DebugListener{}),
 		unleash.WithAppName("my-application"),
-		unleash.WithRefreshInterval(5*time.Second),
-		unleash.WithMetricsInterval(5*time.Second),
+		unleash.WithRefreshInterval(20*time.Second),
+		unleash.WithDisableMetrics(true),
 		unleash.WithStorage(&unleash.BootstrapStorage{Reader: demoReader}),
-		unleash.WithUrl("https://localhost:4242"),
+		unleash.WithUrl("http://foo.com"),
 	)
 
 	if err != nil {
@@ -63,6 +96,8 @@ func Test_withVariantsAndANonExistingStrategyName(t *testing.T) {
 	if feature.Enabled == true {
 		t.Fatalf("Expected feature to be disabled because Environment does not exist as strategy")
 	}
+	err = unleash.Close()
+	a.Nil(err)
 }
 
 func Test_IsEnabledWithUninitializedClient(t *testing.T) {
@@ -70,4 +105,5 @@ func Test_IsEnabledWithUninitializedClient(t *testing.T) {
 	if !result {
 		t.Fatalf("Expected true")
 	}
+
 }
