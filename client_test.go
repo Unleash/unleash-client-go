34874e183f70ea31eb95932f3d49f34e75750366
Christopher Kolstad <chriswk@getunleash.ai>
2023-12-11 11:05:20 +0100

    chore: clean tests usage of endpoints (#158)
    * chore: clean tests usage of endpoints
    
    Previously, once we hit the feature refresh interval, we saw failed
    requests against https://localhost:4242, this update replaces all usages
    in these tests with the gocked url instead of a fake localhost address
    
    ---------
    
    Co-authored-by: Simon Hornby <liquidwicked64@gmail.com>

--- client_test.go
+++ client_test.go
@@ -1,7 +1,6 @@
 package unleash
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/Unleash/unleash-client-go/v4/api"
@@ -35,23 +34,8 @@ func TestClientWithoutListener(t *testing.T) {
 	)
 	assert.Nil(err, "client should not return an error")
 
-	go func() {
-		for {
-			select {
-			case e := <-client.Errors():
-				t.Errorf("Unexpected error: %v", e)
-				return
-			case w := <-client.Warnings():
-				t.Errorf("Unexpected warning: %v", w)
-				return
-			case <-client.Count():
-			case <-client.Sent():
-			}
-		}
-	}()
-	<-client.Registered()
-	<-client.Ready()
-	client.Close()
+	err = client.Close()
+	assert.Nil(err)
 	assert.True(gock.IsDone(), "there should be no more mocks")
 }
 
@@ -635,8 +619,6 @@ func TestClient_WithMultipleSegments(t *testing.T) {
 	assert.NoError(err)
 	client.WaitForReady()
 
-	fmt.Printf("%v", client.repository.segments)
-
 	isEnabled := client.IsEnabled(feature, WithContext(context.Context{
 		Properties: map[string]string{"custom-id": "custom-ctx", "semver": "3.2.2", "age": "18", "domain": "unleashtest"},
 	}))
@@ -752,8 +734,6 @@ func TestClient_VariantShouldRespectConstraint(t *testing.T) {
 	assert.NoError(err)
 	client.WaitForReady()
 
-	fmt.Printf("%v", client.repository.segments)
-
 	variant := client.GetVariant(feature, WithVariantContext(context.Context{
 		Properties: map[string]string{"custom-id": "custom-ctx", "semver": "3.2.2", "age": "18", "domain": "unleashtest"},
 	}))
@@ -868,8 +848,6 @@ func TestClient_VariantShouldFailWhenSegmentConstraintsDontMatch(t *testing.T) {
 
 	assert.NoError(err)
 	client.WaitForReady()
-
-	fmt.Printf("%v", client.repository.segments)
 
 	variant := client.GetVariant(feature, WithVariantContext(context.Context{
 		Properties: map[string]string{"custom-id": "custom-ctx", "semver": "3.2.2", "age": "18", "domain": "unleashtest"},
--- unleash_test.go
+++ unleash_test.go
@@ -1,16 +1,37 @@
 package unleash_test
 
 import (
-	"fmt"
+	"io/ioutil"
 	"os"
 	"testing"
 	"time"
 
 	"github.com/Unleash/unleash-client-go/v4"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/h2non/gock.v1"
 )
 
 func Test_withVariants(t *testing.T) {
+	a := assert.New(t)
 	demoReader, err := os.Open("demo_app_toggles.json")
+	if err != nil {
+		t.Fail()
+	}
+	defer gock.OffAll()
+	defer demoReader.Close()
+
+	gock.New("http://foo.com").
+		Post("/client/register").
+		Reply(200)
+
+	jsonStr, err := read_demo_app_toggles()
+	if err != nil {
+		t.Fail()
+	}
+
+	// Use the string as the body of the Gock request
+	gock.New("http://foo.com").
+		Get("/client/features").Reply(200).BodyString(jsonStr)
 	if err != nil {
 		t.Fail()
 	}
@@ -18,9 +39,9 @@ func Test_withVariants(t *testing.T) {
 		unleash.WithListener(&unleash.DebugListener{}),
 		unleash.WithAppName("my-application"),
 		unleash.WithRefreshInterval(5*time.Second),
-		unleash.WithMetricsInterval(5*time.Second),
+		unleash.WithDisableMetrics(true),
 		unleash.WithStorage(&unleash.BootstrapStorage{Reader: demoReader}),
-		unleash.WithUrl("https://localhost:4242"),
+		unleash.WithUrl("http://foo.com"),
 	)
 
 	if err != nil {
@@ -28,7 +49,6 @@ func Test_withVariants(t *testing.T) {
 	}
 
 	feature := unleash.GetVariant("Demo")
-	fmt.Printf("feature is %v\n", feature)
 	if feature.Enabled == false {
 		t.Fatalf("Expected feature to be enabled")
 	}
@@ -38,20 +58,46 @@ func Test_withVariants(t *testing.T) {
 	if feature.Payload.Value != "35" && feature.Payload.Value != "55" {
 		t.Fatalf("Expected one of the variant payloads")
 	}
+	err = unleash.Close()
+	a.Nil(err)
+}
+
+func read_demo_app_toggles() (string, error) {
+	demoReader, err := os.Open("demo_app_toggles.json")
+	if err != nil {
+		return "", err
+	}
+	defer demoReader.Close()
+	byteValue, _ := ioutil.ReadAll(demoReader)
+	return string(byteValue), nil
 }
 
 func Test_withVariantsAndANonExistingStrategyName(t *testing.T) {
+	a := assert.New(t)
 	demoReader, err := os.Open("demo_app_toggles.json")
 	if err != nil {
 		t.Fail()
 	}
+	defer gock.OffAll()
+
+	gock.New("http://foo.com").
+		Post("/client/register").
+		Reply(200)
+	jsonStr, err := read_demo_app_toggles()
+	if err != nil {
+		t.Fail()
+	}
+
+	// Use the string as the body of the Gock request
+	gock.New("http://foo.com").
+		Get("/client/features").Reply(200).BodyString(jsonStr)
 	err = unleash.Initialize(
 		unleash.WithListener(&unleash.DebugListener{}),
 		unleash.WithAppName("my-application"),
-		unleash.WithRefreshInterval(5*time.Second),
-		unleash.WithMetricsInterval(5*time.Second),
+		unleash.WithRefreshInterval(20*time.Second),
+		unleash.WithDisableMetrics(true),
 		unleash.WithStorage(&unleash.BootstrapStorage{Reader: demoReader}),
-		unleash.WithUrl("https://localhost:4242"),
+		unleash.WithUrl("http://foo.com"),
 	)
 
 	if err != nil {
@@ -59,10 +105,11 @@ func Test_withVariantsAndANonExistingStrategyName(t *testing.T) {
 	}
 
 	feature := unleash.GetVariant("AuditLog")
-	fmt.Printf("feature is %v\n", feature)
 	if feature.Enabled == true {
 		t.Fatalf("Expected feature to be disabled because Environment does not exist as strategy")
 	}
+	err = unleash.Close()
+	a.Nil(err)
 }
 
 func Test_IsEnabledWithUninitializedClient(t *testing.T) {
@@ -70,4 +117,5 @@ func Test_IsEnabledWithUninitializedClient(t *testing.T) {
 	if !result {
 		t.Fatalf("Expected true")
 	}
+
 }
