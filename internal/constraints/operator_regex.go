2cca803559666ae032e8d44fbbe2a7e36bc32d7d
Simon Hornby <liquidwicked64@gmail.com>
2026-02-26 10:30:24 +0200

    chore: add lru cache on regex constraint to reduce sting on compile times

--- internal/constraints/operator_regex.go
+++ internal/constraints/operator_regex.go
@@ -2,16 +2,48 @@ package constraints
 
 import (
 	"regexp"
+	"sync"
+
+	lru "github.com/hashicorp/golang-lru/v2"
 
 	"github.com/Unleash/unleash-go-sdk/v6/api"
 	"github.com/Unleash/unleash-go-sdk/v6/context"
 )
 
+type regexKey struct {
+	pattern string
+	ci      bool
+}
+
+var (
+	regexCache     *lru.Cache[regexKey, *regexp.Regexp]
+	regexCacheOnce sync.Once
+)
+
+func getRegexCache() *lru.Cache[regexKey, *regexp.Regexp] {
+	regexCacheOnce.Do(func() {
+		cache, _ := lru.New[regexKey, *regexp.Regexp](100)
+		regexCache = cache
+	})
+	return regexCache
+}
+
 func operatorRegex(ctx *context.Context, constraint api.Constraint) bool {
 	contextValue := ctx.Field(constraint.ContextName)
 
-	pattern := constraint.Value
-	if constraint.CaseInsensitive {
+	key := regexKey{
+		pattern: constraint.Value,
+		ci:      constraint.CaseInsensitive,
+	}
+
+	cache := getRegexCache()
+
+	if re, ok := cache.Get(key); ok {
+		return re.MatchString(contextValue)
+	}
+
+	pattern := key.pattern
+	if key.ci {
 		pattern = "(?i:" + pattern + ")"
 	}
 
@@ -19,6 +51,8 @@ func operatorRegex(ctx *context.Context, constraint api.Constraint) bool {
 	if err != nil {
 		return false
 	}
+
+	cache.Add(key, re)
 
 	return re.MatchString(contextValue)
 }
